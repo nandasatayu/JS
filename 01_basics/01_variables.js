2ea486109841697d27f95f4254b76ae5484e6de1
Satayu Nanda <151006783+nandasatayu@users.noreply.github.com>
2024-11-14 13:52:45 +0000

    reaning variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accoundId = 144556
+let accountEmail = "satayunanda@gmail.com"
+var accountPassword = "12345"
+accountCity = "Kolkata"
+
+//changing of constant values is not allowed
+
+
+accountEmail = "nanda@gamil.com"
+accountPassword="8797"
+accountCity = "Mumbai"
+console.table([accountEmail, accountPassword, accountCity])
+
+
+// Do not use var keyword to initialise variables, as it does not take into consideration "Scope"
+
