652e139aaba2e799605522bfbd30a1f01a0b7234
Satayu Nanda <151006783+nandasatayu@users.noreply.github.com>
2024-11-14 14:09:53 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,10 @@
+"use strict"; // use all code as newer version
+ // code readibility should be high
+ let age = 18
+ let name = "Satayu"
+ let isLoggedin = false
+ let state
+// age is Number
+// name is String
+// isloggedin is a Boolean
+// state is undefined
